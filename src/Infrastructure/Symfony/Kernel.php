c7475b97f5bc62cc9d6e73521d050de52997e6d0
Seb <seb@hostme.fr>
2025-10-24 13:42:55 +0200

    Initialize project with DDD structure

--- src/Infrastructure/Symfony/Kernel.php
+++ src/Infrastructure/Symfony/Kernel.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App;
+declare(strict_types=1);
+
+namespace Infrastructure\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
