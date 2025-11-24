e95c71e884050b6219b5235702849446ac19fcf5
seb <s.dieunidou@gmail.com>
2025-11-24 10:02:01 +0100

    test mistral model

--- src/Infrastructure/Auth/Symfony/Controller/LoginController.php
+++ src/Infrastructure/Auth/Symfony/Controller/LoginController.php
@@ -35,7 +35,7 @@ final class LoginController extends AbstractController
         // lol
         // lol 2
         // lozlzn
-        // test mistral instruct
+        // test mistral instruc
     }
 
     public function __invoke(#[MapRequestPayload] LoginRequest $loginRequest, Request $request): JsonResponse
