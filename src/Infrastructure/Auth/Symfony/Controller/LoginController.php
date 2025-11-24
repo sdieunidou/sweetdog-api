f95e2331cb5d76b742bcc75d9d5c31c87d7aebda
seb <s.dieunidou@gmail.com>
2025-11-24 19:32:36 +0100

    test ci

--- src/Infrastructure/Auth/Symfony/Controller/LoginController.php
+++ src/Infrastructure/Auth/Symfony/Controller/LoginController.php
@@ -32,6 +32,7 @@ final class LoginController extends AbstractController
         // Initialisation du cache directement dans le contrôleur
         $this->cache = new FilesystemAdapter();
         // test
+        // test 4
     }
 
     public function __invoke(#[MapRequestPayload] LoginRequest $loginRequest, Request $request): JsonResponse
